852f2b67cbd38a2b8a7760c42f9079d452e75cbc
Kai(luo) Wang <kailuo.wang@gmail.com>
2018-12-05 13:29:44 -0500

    [WIP] make api more parametric (#25)
    * make api more parametric
    
    * added some tests

---
+++ analysis/src/test/scala/com/iheart/thomas/analysis/NumericGroupResultSuite.scala
@@ -0,0 +1,26 @@
+package com.iheart.thomas.analysis
+
+import syntax.AllSyntax
+import org.scalatest.{FunSuite, Matchers}
+
+class NumericGroupResultSuite extends FunSuite with Matchers with AllSyntax {
+  test("probability of Improvement") {
+    val subject = NumericGroupResult(List(-1, 1, -3, 2, -3))
+    subject.probabilityOfImprovement shouldBe Probability(0.4d)
+  }
+
+  test("riskOfUsing") {
+    val subject = NumericGroupResult((-6 to 93).toList.map(_.toDouble))
+    subject.riskOfUsing shouldBe -2d
+  }
+
+  test("riskOf Not Using") {
+    val subject = NumericGroupResult((-50 to 50).toList.map(_.toDouble))
+    subject.riskOfNotUsing shouldBe -44d
+  }
+
+  test("expected effect") {
+    val subject = NumericGroupResult((-50 to 50).toList.map(_.toDouble))
+    subject.expectedEffect shouldBe 0d
+  }
+}
