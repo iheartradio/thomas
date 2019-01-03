699693e71bb00bb845e52cae6a9cee24e4a82426
Kailuo Wang <kailuo.wang@gmail.com>
2019-01-03 12:49:14 -0500

    wip update rainier

--- analysis/src/main/scala/com/iheart/thomas/analysis/syntax/RandomVariableSyntax.scala
+++ analysis/src/main/scala/com/iheart/thomas/analysis/syntax/RandomVariableSyntax.scala
@@ -2,13 +2,13 @@ package com.iheart.thomas
 package analysis
 package syntax
 
-import com.stripe.rainier.core.{RandomVariable, Sampleable}
+import com.stripe.rainier.core.{RandomVariable, ToGenerator}
 import com.stripe.rainier.sampler.RNG
 
 trait RandomVariableSyntax {
   implicit class RandomVariableOps[T](private val rv: RandomVariable[T]) {
     def sample[V](sampleSettings: SampleSettings)(
-      implicit rng: RNG, sampleable: Sampleable[T, V]): List[V] = {
+      implicit rng: RNG, tg: ToGenerator[T, V]): List[V] = {
       import sampleSettings._
       rv.sample(sampler,
         warmupIterations = warmupIterations,
