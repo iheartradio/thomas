9ec0181b9ab2aa16fb2dfcb60ab6017d2ff483a0
Kailuo Wang <kailuo.wang@gmail.com>
2020-05-06 16:42:23 -0400

    fix build

--- testkit/src/main/scala/com/iheart/thomas/testkit/Factory.scala
+++ testkit/src/main/scala/com/iheart/thomas/testkit/Factory.scala
@@ -2,15 +2,7 @@ package com.iheart.thomas.testkit
 
 import java.time.OffsetDateTime
 
-import com.iheart.thomas.abtest.model.{
-  AbtestSpec,
-  Group,
-  GroupMetas,
-  GroupRange,
-  MetaFieldName,
-  Tag,
-  UserMeta
-}
+import com.iheart.thomas.abtest.model._
 
 import scala.util.Random
 
@@ -23,7 +15,7 @@ object Factory {
       feature: String = "AMakeUpFeature" + Random.alphanumeric.take(5).mkString,
       alternativeIdName: Option[MetaFieldName] = None,
       groups: List[Group] = List(Group("A", 0.5), Group("B", 0.5)),
-      matchingUserMeta: UserMeta = Map(),
+      userMetaCriteria: UserMetaCriteria = None,
       segRanges: List[GroupRange] = Nil,
       requiredTags: List[Tag] = Nil,
       groupMetas: GroupMetas = Map()
@@ -35,7 +27,7 @@ object Factory {
     end = Some(now.plusDays(end.toLong)),
     groups = groups,
     alternativeIdName = alternativeIdName,
-    userMetaCriteria = matchingUserMeta,
+    userMetaCriteria = userMetaCriteria,
     segmentRanges = segRanges,
     requiredTags = requiredTags,
     groupMetas = groupMetas
