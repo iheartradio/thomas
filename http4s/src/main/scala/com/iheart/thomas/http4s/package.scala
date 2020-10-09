5ff4d98effa29f565d903b4c20ae6999deae291f
Kai(luo) Wang <kailuo.wang@gmail.com>
2020-10-09 10:54:34 -0400

    added initial role configurable (#333)
    * added initial role configurable
    
    * fixing build

--- http4s/src/main/scala/com/iheart/thomas/http4s/package.scala
+++ http4s/src/main/scala/com/iheart/thomas/http4s/package.scala
@@ -8,10 +8,12 @@ import tsec.mac.jca.HMACSHA256
 package object http4s {
   implicit object Roles extends SimpleAuthEnum[Role, String] {
     val Admin: Role = Role("Admin")
-    val Developer: Role = Role("Developer")
-    val Reader: Role = Role("Reader")
+    val Developer: Role =
+      Role("Developer") //can start their own test and become feature admin
+    val Tester: Role = Role("Tester") //can change overrides
+    val User: Role = Role("User") //readonly but can be feature admin
 
-    override val values: AuthGroup[Role] = AuthGroup(Admin, Reader, Developer)
+    override val values: AuthGroup[Role] = AuthGroup(Admin, User, Developer)
 
     override def getRepr(t: Role): String = t.name
 
