498d35ee9bb5a512b7085feb18a1933a3f62605b
Kai(luo) Wang <kailuo.wang@gmail.com>
2020-10-27 15:26:57 -0400

    improve login process (#346)

--- http4s/src/main/scala/com/iheart/thomas/http4s/ReverseRoutes.scala
+++ http4s/src/main/scala/com/iheart/thomas/http4s/ReverseRoutes.scala
@@ -2,8 +2,10 @@ package com.iheart.thomas.http4s
 
 class ReverseRoutes(rootPath: String) {
   val tests = s"$rootPath/tests"
+  val home = tests
   val features = s"$rootPath/features"
-  val login = s"$rootPath/login"
+  def login(redirectTo: String): String = s"$login?redirectTo=$redirectTo"
+  val login: String = s"$rootPath/login"
   val users = s"$rootPath/users"
   val register = s"$rootPath/register"
   val logout = s"$rootPath/logout"
