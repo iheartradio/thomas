6c56ad25c25c98cae45004608648ea7fbd9212de
Kailuo Wang <kailuo.wang@gmail.com>
2022-02-11 11:56:18 -0500

    added more tests

--- tests/src/test/scala/com/iheart/thomas/analysis/bayesian/models/BetaKPISuite.scala
+++ tests/src/test/scala/com/iheart/thomas/analysis/bayesian/models/BetaKPISuite.scala
@@ -24,8 +24,8 @@ class BetaKPISuite extends AsyncFreeSpec with AsyncIOSpec with Matchers {
         .evaluate(
           BetaModel(200d, 300d),
           Map(
-            "A" -> Conversions(200L, 300L),
             "B" -> Conversions(250L, 300L),
+            "A" -> Conversions(200L, 300L),
             "C" -> Conversions(265L, 300L),
             "D" -> Conversions(230L, 300L)
           ),
@@ -38,5 +38,63 @@ class BetaKPISuite extends AsyncFreeSpec with AsyncIOSpec with Matchers {
         }
 
     }
+
+    "can evaluate optimal group distribution with large sample size" in {
+      alg
+        .evaluate(
+          BetaModel(0d, 0d),
+          Map(
+            "A" -> Conversions(7000L, 10000L),
+            "B" -> Conversions(6800, 10000L),
+            "C" -> Conversions(6700L, 10000L),
+            "D" -> Conversions(7500L, 10000L)
+          ),
+          None
+        )
+        .asserting { dist =>
+          dist
+            .sortBy(_.probabilityBeingOptimal.p)
+            .map(_.name).last shouldBe  "D"
+        }
+    }
+
+    "can evaluate optimal group distribution with asymmetric sample size" in {
+      alg
+        .evaluate(
+          BetaModel(0d, 0d),
+          Map(
+            "A" -> Conversions(70L, 100L),
+            "B" -> Conversions(680, 1000L),
+            "C" -> Conversions(6700L, 10000L),
+            "D" -> Conversions(750L, 1000L)
+          ),
+          None
+        )
+        .asserting { dist =>
+          println(dist)
+          dist
+            .sortBy(_.probabilityBeingOptimal.p)
+            .map(_.name).last shouldBe  "D"
+        }
+    }
+
+    "can evaluate optimal group distribution with real data" in {
+      alg
+        .evaluate(
+          BetaModel(1414L, 500L),
+          Map(
+            "A" -> Conversions(1414L, 1973L),
+            "C" -> Conversions(20985L, 31267L)
+          ),
+          None
+        )
+        .asserting { dist =>
+          println(dist)
+          dist
+            .sortBy(_.probabilityBeingOptimal.p)
+            .map(_.name).last shouldBe  "A"
+        }
+    }
+
   }
 }
