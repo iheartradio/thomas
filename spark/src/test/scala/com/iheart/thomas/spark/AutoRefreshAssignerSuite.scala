eeb5145d6a2aa59de503977dba34594ce1a20489
Kailuo Wang <kailuo.wang@gmail.com>
2022-03-16 11:05:02 -0400

    improved test

--- spark/src/test/scala/com/iheart/thomas/spark/AutoRefreshAssignerSuite.scala
+++ spark/src/test/scala/com/iheart/thomas/spark/AutoRefreshAssignerSuite.scala
@@ -1,14 +1,22 @@
-// disable this test to see if this is still required since it seems that the whole UDF is no longer serializable.
-//package com.iheart.thomas.spark
-//import cats.kernel.laws.discipline.SerializableTests
-//import cats.tests.CatsSuite
-//import concurrent.duration._
-//
-//class AutoRefreshAssignerSuite extends CatsSuite {
-//  checkAll(
+
+package com.iheart.thomas.spark
+import cats.kernel.laws.discipline.SerializableTests
+import cats.tests.CatsSuite
+import concurrent.duration._
+
+class AutoRefreshAssignerSuite extends CatsSuite {
+  /// disable this test to see if this is still required since it seems that the whole UDF is no longer serializable.
+  //  checkAll(
 //    "AutoRefreshAssigner.udf Serializable",
 //    SerializableTests.serializable(
 //      AutoRefreshAssigner("fakeUrl", 10.seconds).assignUdf("fakeFeature")
 //    )
 //  )
-//}
+
+  checkAll(
+    "AutoRefreshAssigner.udf Serializable",
+    SerializableTests.serializable(
+      AutoRefreshAssigner("fakeUrl", 10.seconds).assignFunction("fakeFeature")
+    )
+  )
+}
