e919e576ee7855b18e6439518ae66b45a7f37e5c
Kailuo Wang <kailuo.wang@gmail.com>
2022-10-12 12:36:01 -0400

    enable configurable doc url

--- http4s/src/main/scala/com/iheart/thomas/http4s/UIConfig.scala
+++ http4s/src/main/scala/com/iheart/thomas/http4s/UIConfig.scala
@@ -6,12 +6,13 @@ import com.iheart.thomas.http4s.AdminUI.AdminUIConfig
 case class UIEnv(
     routes: ReverseRoutes,
     currentUser: User,
-    siteName: String)
+    siteName: String,
+    docUrl: String)
 
 object UIEnv {
   def apply(
       currentUser: User
     )(implicit cfg: AdminUIConfig
     ): UIEnv =
-    UIEnv(ReverseRoutes(cfg.rootPath), currentUser, cfg.siteName)
+    UIEnv(ReverseRoutes(cfg.rootPath), currentUser, cfg.siteName, cfg.docUrl)
 }
