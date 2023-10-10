72dfcc4e394a56ea6d8f75a385f2ac10aba27f58
Tony Liu <tonylove5251@gmail.com>
2023-10-10 10:45:14 -0700

    update tests

--- tests/src/test/scala/com/iheart/thomas/abtest/model/ModelSuite.scala
+++ tests/src/test/scala/com/iheart/thomas/abtest/model/ModelSuite.scala
@@ -2,51 +2,45 @@ package com.iheart.thomas
 package abtest
 package model
 
-import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.funsuite.AnyFunSuiteLike
 import org.scalatest.matchers.should.Matchers
-import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+
 import java.time.Instant
 
-class ModelSuite extends AnyFreeSpec
-  with ScalaCheckDrivenPropertyChecks
-  with Matchers {
+class ModelSuite extends AnyFunSuiteLike with Matchers {
 
-  "Feature tests" - {
-    val existingFeatureConfig: Feature = Feature(
+  val existingFeatureConfig: Feature = Feature(
+    name = "test feature",
+    overrides = Map(
+      "user1" -> "control",
+      "user2" -> "treatment"
+    ),
+    lastUpdated = Some(Instant.EPOCH)
+  )
+
+  test("a new feature config with new override should not trigger nonTestSettingsChangedFrom") {
+    val newConfig: Feature = Feature(
       name = "test feature",
+      overrides = Map(
+        "user1" -> "treatment",
+        "user2" -> "control"
+      ),
+      lastUpdated = None
+    )
+    existingFeatureConfig nonTestSettingsChangedFrom newConfig shouldBe false
+  }
+
+  test("a new feature config with new description should trigger nonTestSettingsChangedFrom") {
+    val newConfig: Feature = Feature(
+      name = "test feature",
+      description = Some("test description"),
       overrides = Map(
         "user1" -> "control",
         "user2" -> "treatment"
       ),
-      lastUpdated = Some(Instant.now())
+      lastUpdated = None
     )
-
-    "a new feature config with new override should not trigger nonTestSettingsChangedFrom" - {
-      val newConfig: Feature = Feature(
-        name = "test feature",
-        overrides = Map(
-          "user1" -> "treatment",
-          "user2" -> "control"
-        ),
-        lastUpdated = None
-      )
-      existingFeatureConfig nonTestSettingsChangedFrom newConfig  shouldBe false
-    }
-
-    "a new feature config with new description should trigger nonTestSettingsChangedFrom" - {
-      val newConfig: Feature = Feature(
-        name = "test feature",
-        description = Some("test description"),
-        overrides = Map(
-          "user1" -> "control",
-          "user2" -> "treatment"
-        ),
-        lastUpdated = None
-      )
-      existingFeatureConfig nonTestSettingsChangedFrom newConfig shouldBe true
-    }
-
-
-
+    existingFeatureConfig nonTestSettingsChangedFrom newConfig shouldBe true
   }
+
 }
